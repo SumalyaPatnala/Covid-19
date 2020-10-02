5f4e4cf6079d5287e43beb8f8291780f2d273e4a
SumalyaPatnala <60100821+SumalyaPatnala@users.noreply.github.com>
2020-10-02 19:19:03 +0530

    Add files via upload

--- dynamic_mapping_of_covid.py
+++ dynamic_mapping_of_covid.py
@@ -1,23 +1,52 @@
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
+import PIL
+import io
 
-#reading in the csv file
-data_india = pd.read_csv('Covid-19_Across_India.csv')
+india_Covid_df = pd.read_csv('india_Covid_Dated.csv',index_col='Date')
 
-#converting the available data into time-series
-india_Covid_df= data_india.pivot_table('Confirmed', index='Date',
-                                                        columns='State/UnionTerritory',fill_value=0)
-
-india_Covid_df=india_Covid_df.T
+india_Covid_df_dated=india_Covid_df.T
 
 #reading in the Indian map shape file 
 india = gpd.read_file(r'D:\COVID-19-master\csse_covid_19_data\csse_covid_19_time_series\India_States.shp')
 
-#checking state name discrepancies
-for index, row in india_Covid_df.iterrows():
-    if index not in india['state_name'].to_list():
-        print(index + " is not in the shape file")
-        
-    else:
-        pass
+
+#merging the 'india_Covid_df' and 'india' geopandas data frame
+merge = india.join(india_Covid_df_dated, on='state_name', how='right')
+
+image_frames = []
+
+for dates in merge.columns.to_list()[9:253]:
+    
+    #plot
+    ax = merge.plot(column = dates,
+                    cmap = 'OrRd',
+                    figsize = (14,14),
+                    legend = True,
+                    scheme = 'user_defined',
+                    classification_kwds = {'bins':[10,20,50,100,500,1000,5000,10000,500000,1000000,5000000]},
+                    edgecolor = 'black',
+                    linewidth = 0.4)
+    
+    # Add a title to the map
+    ax.set_title('Total Confirmed Coronavirus Cases: '+dates, fontdict = {'fontsize': 20}, pad=12.5)
+    
+    # Removing the axes
+    ax.set_axis_off()
+    
+    # move the legend
+    ax.get_legend().set_bbox_to_anchor((0.18,0.4))
+    
+    img = ax.get_figure()
+    f=io.BytesIO()
+    img.savefig(f, format = 'png', bbox_inches = 'tight')
+    f.seek(0)
+    image_frames.append(PIL.Image.open(f))
+    
+f.close()
+
+#creating a gif animation
+image_frames[0].save("Covid-19 Dynamics across Indian States.gif", format = 'GIF',
+                     append_images = image_frames[1:],
+                     save_all = True, duration = 300,
+                     loop = 3)
